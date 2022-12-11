7666d5e062905a211a75dcc45d68fdd4e261d8b0
Petr Prokhorenkov <prokhorenkov@gmail.com>
2022-12-11 23:27:17 +0100

    wip

--- 11.py
+++ 11.py
@@ -1,13 +1,13 @@
 ii = __import__
-op = ii("operator")
 
-mul = op.mul
+i = lambda x: x
+k = lambda k: lambda _: k
+s = lambda f: lambda g: lambda x: f(x)(y(x))
+l0 = list()
 
 ite = lambda i, t, e: [e, t][i]()
-id = lambda x: x
-const = lambda k: lambda _: k
 
-xx = ite(False, lambda: (lambda x: x // 3, 20), lambda: (id, 10000))
+xx = ite(False, lambda: (lambda x: x // 3, 20), lambda: (i, 10000))
 
 last_int = lambda x: int(x.split()[-1])
 
@@ -30,7 +30,7 @@ read_f = lambda self: lambda result: lambda lines: ite(
                 last_int(lines[3]),
                 last_int(lines[4]),
                 last_int(lines[5]),
-                0]),
+                []]),
                 self(self)(result)(lines[7:]))
             )(lines[2].split("= ")[1].split()[1:]),
         lambda: None)
@@ -40,36 +40,34 @@ times = lambda d, xs, ys: mapl(lambda ks: sum(map(lambda tt: tt[0]*tt[1],zip(ks,
 ns = lambda n, xs: [n]*len(xs)
 test = lambda xs, ys: mapl(lambda ww: ww[0] % ww[1] == 0, zip(xs, ys))
 
-def round(monkeys, d):
-    for m in monkeys:
-        next = mapl(xx[0], times(d, zip(map(sqr, m[0]), m[0], ns(1, m[0])), m[1]))
-        t = test(next, ns(m[2], next))
-        mapl(
+round = lambda monkeys, d: mapl(
+    lambda m: (
+        (lambda next: mapl(
             lambda zz: monkeys[ite(zz[1], lambda: m[3], lambda: m[4])][0].append(zz[0]),
-            zip(next, t))
-        m[5] += len(m[0])
-        m[0].clear()
+            zip(next, test(next, ns(m[2], next)))))(mapl(xx[0], times(d, zip(map(sqr, m[0]), m[0], ns(1, m[0])), m[1]))),
+        m[5].append(len(m[0])),
+        m[0].clear()),
+    monkeys)
 
-monkeys = []
-read(monkeys)(list(ii("sys").stdin))
-d = ii("math").lcm(*mapl(lambda bv: bv[2], monkeys))
-mapl(lambda xxx: round(monkeys, d), range(xx[1]))
+(lambda monkeys: (
+    read(monkeys)(list(ii("sys").stdin)),
+    mapl(lambda xxx: round(monkeys, ii("math").lcm(*mapl(lambda bv: bv[2], monkeys))), range(xx[1])),
+    print((lambda maxes: maxes[0]*maxes[1])(fix(
+        lambda self: lambda acc: lambda xs: ite(
+        len(xs) == 0,
+        lambda: acc,
+        lambda: (lambda cont:
+            ite(
+                xs[0] < acc[0],
+                lambda: cont(acc),
+                lambda: ite(
+                    xs[0] < acc[1],
+                    lambda: cont([xs[0], acc[1]]),
+                    lambda: cont([acc[1], xs[0]])
+                )
 
-max2 = lambda self: lambda acc: lambda xs: ite(
-    len(xs) == 0,
-    lambda: acc,
-    lambda: (lambda cont:
-        ite(
-            xs[0] < acc[0],
-            lambda: cont(acc),
-            lambda: ite(
-                xs[0] < acc[1],
-                lambda: cont([xs[0], acc[1]]),
-                lambda: cont([acc[1], xs[0]])
             )
-
-        )
-    )(lambda new_acc: self(self)(new_acc)(xs[1:]))
-)
-
-print((lambda maxes: maxes[0]*maxes[1])(fix(max2)([-2, -1])(mapl(lambda www:www[5], monkeys))))
+        )(lambda new_acc: self(self)(new_acc)(xs[1:]))
+    )
+    )([-2, -1])(mapl(lambda www:sum(www[5]), monkeys))))
+))(list())
