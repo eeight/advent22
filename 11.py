200cb8ca4b5f533c5ea3573bf8eced05912183cc
Petr Prokhorenkov <prokhorenkov@gmail.com>
2022-12-12 00:04:03 +0100

    wip

--- 11.py
+++ 11.py
@@ -1,15 +1,35 @@
 ii = __import__
 
+bind_2nd = lambda f, y: lambda x: f(x, y)
+op = ii("operator")
+at = getattr(op, "getitem")
+_0 = len(list())
+_1 = 1 # XXX
+at0 = bind_2nd(at, _0)
+at1 = bind_2nd(at, _1)
+tail = bind_2nd(at, slice(1, 999))
+tail7 = bind_2nd(at, slice(7, 999))
+
+
 i = lambda x: x
 k = lambda k: lambda _: k
 s = lambda f: lambda g: lambda x: f(x)(y(x))
 l0 = list()
 
-ite = lambda i, t, e: [e, t][i]()
+ite = lambda i, t, e: at((e, t), i)()
+
+# ite = \i t e . ap (at (tuple e t) i) None
+# ite = \e . (at (tuple e t) i) (K None)
+# ite = S (\e. at (tuple e t) i) (K None)
+# \e. (at (tuple e t)) i
+# S (\e. at (tuple e t)) (K i)
+# S (S (K at) (S (S (K tuple) I) (K t))) (K i)
+lambda i, t, e: at((e, t), i)()
+
 
 xx = ite(False, lambda: (lambda x: x // 3, 20), lambda: (i, 10000))
 
-last_int = lambda x: int(x.split()[-1])
+last_int = lambda x: int(at(x.split(), -1))
 
 fix = lambda f: f(f)
 
@@ -19,55 +39,55 @@ sqr = lambda x: x*x
 read_f = lambda self: lambda result: lambda lines: ite(
         len(lines) > 5,
             lambda: (lambda yy: (
-                result.append([
-                mapl(int, lines[1].split(": ")[1].split(", ")),
-                ite(yy[0] == '*',
+                result.append(list((
+                mapl(int, at1(at1(lines).split(": ")).split(", ")),
+                ite(at0(yy) == "*",
                     lambda: ite(
-                        yy[1] == 'old',
-                        lambda: [1, 0, 0],
-                        lambda: [0, int(yy[1]), 0]),
-                    lambda: [0, 1, int(yy[1])]),
-                last_int(lines[3]),
-                last_int(lines[4]),
-                last_int(lines[5]),
-                []]),
-                self(self)(result)(lines[7:]))
-            )(lines[2].split("= ")[1].split()[1:]),
+                        at1(yy) == "old",
+                        lambda: (_1, _0, _0),
+                        lambda: (_0, int(at1(yy)), _0)),
+                    lambda: (_0, 1, int(at1(yy)))),
+                last_int(at(lines, 3)),
+                last_int(at(lines, 4)),
+                last_int(at(lines, 5)),
+                list()))),
+                self(self)(result)(tail7(lines)))
+            )(tail(at1(at(lines, 2).split("= ")).split())),
         lambda: None)
 
 read = fix(read_f)
-times = lambda d, xs, ys: mapl(lambda ks: sum(map(lambda tt: tt[0]*tt[1],zip(ks, ys))) % d, xs)
-ns = lambda n, xs: [n]*len(xs)
-test = lambda xs, ys: mapl(lambda ww: ww[0] % ww[1] == 0, zip(xs, ys))
+times = lambda d, xs, ys: mapl(lambda ks: sum(map(lambda tt: at0(tt)*at1(tt),zip(ks, ys))) % d, xs)
+ns = lambda n, xs: (n, )*len(xs)
+test = lambda xs, ys: mapl(lambda ww: at0(ww) % at1(ww) == _0, zip(xs, ys))
 
 round = lambda monkeys, d: mapl(
     lambda m: (
         (lambda next: mapl(
-            lambda zz: monkeys[ite(zz[1], lambda: m[3], lambda: m[4])][0].append(zz[0]),
-            zip(next, test(next, ns(m[2], next)))))(mapl(xx[0], times(d, zip(map(sqr, m[0]), m[0], ns(1, m[0])), m[1]))),
-        m[5].append(len(m[0])),
-        m[0].clear()),
+            lambda zz: at0(at(monkeys, at(m, 4 - at1(zz)))).append(at0(zz)),
+            zip(next, test(next, ns(at(m, 2), next)))))(mapl(at0(xx), times(d, zip(map(sqr, at0(m)), at0(m), ns(1, at0(m))), at1(m)))),
+        at(m, 5).append(len(at0(m))),
+        at0(m).clear()),
     monkeys)
 
 (lambda monkeys: (
     read(monkeys)(list(ii("sys").stdin)),
-    mapl(lambda xxx: round(monkeys, ii("math").lcm(*mapl(lambda bv: bv[2], monkeys))), range(xx[1])),
-    print((lambda maxes: maxes[0]*maxes[1])(fix(
+    mapl(lambda xxx: round(monkeys, ii("math").lcm(*mapl(lambda bv: at(bv, 2), monkeys))), range(at1(xx))),
+    print((lambda maxes: at0(maxes)*at1(maxes))(fix(
         lambda self: lambda acc: lambda xs: ite(
-        len(xs) == 0,
+        len(xs) == _0,
         lambda: acc,
         lambda: (lambda cont:
             ite(
-                xs[0] < acc[0],
+                at0(xs) < at0(acc),
                 lambda: cont(acc),
                 lambda: ite(
-                    xs[0] < acc[1],
-                    lambda: cont([xs[0], acc[1]]),
-                    lambda: cont([acc[1], xs[0]])
+                    at0(xs) < at1(acc),
+                    lambda: cont((at0(xs), at1(acc))),
+                    lambda: cont((at1(acc), at0(xs)))
                 )
 
             )
-        )(lambda new_acc: self(self)(new_acc)(xs[1:]))
+        )(lambda new_acc: self(self)(new_acc)(tail(xs)))
     )
-    )([-2, -1])(mapl(lambda www:sum(www[5]), monkeys))))
+    )((-2, -1))(mapl(lambda www:sum(at(www, 5)), monkeys))))
 ))(list())
